be32b3d4f0a3d7e700cfce5b04a2b7a5d3a61af8
ychebotarev <ychebotarev@gmail.com>
2022-07-17 10:48:01 -0700

    modified tests

--- tests/test_linq.py
+++ tests/test_linq.py
@@ -1,112 +1,121 @@
 from yutils.linq import Iteratable
 import pytest
 
+
 def test_construct():
     with pytest.raises(TypeError):
         Iteratable(False).first()
 
+
 def test_get_item():
-    it=Iteratable(range(10))
+    it = Iteratable(range(10))
     for i in range(10):
-        assert it[i]==i
+        assert it[i] == i
+
 
 def test_to_list():
-    result = Iteratable([1,2,3,4]).to_list()
+    result = Iteratable([1, 2, 3, 4]).to_list()
     assert isinstance(result, list)
-    assert len(result)==4
-    assert sum(result)==10
+    assert len(result) == 4
+    assert sum(result) == 10
+
 
 def test_select():
-    result= Iteratable([1,2,3,4])\
-            .select(lambda x: x*2)\
-            .to_list()
-    assert len(result)==4
-    assert sum(result)==1*2+2*2+3*2+4*2
+    result = Iteratable([1, 2, 3, 4]).select(lambda x: x * 2).to_list()
+    assert len(result) == 4
+    assert sum(result) == 1 * 2 + 2 * 2 + 3 * 2 + 4 * 2
+
 
 def test_where():
-    result=Iteratable([1,2,3,4])\
-           .select(lambda x: x*2)\
-           .where(lambda x: x>2)\
-           .to_list()
-    assert len(result)==3
-    assert sum(result)==2*2+3*2+4*2
+    result = (
+        Iteratable([1, 2, 3, 4])
+        .select(lambda x: x * 2)
+        .where(lambda x: x > 2)
+        .to_list()
+    )
+    assert len(result) == 3
+    assert sum(result) == 2 * 2 + 3 * 2 + 4 * 2
+
 
 def test_count():
-    it=Iteratable(range(10))
+    it = Iteratable(range(10))
     assert it.count() == 10
-    assert it.count_if(lambda x: x%2==0) == 5
+    assert it.count_if(lambda x: x % 2 == 0) == 5
+
 
 def test_first():
-    it=Iteratable(range(10))
+    it = Iteratable(range(10))
     assert it.first() == 0
-    assert it.first(lambda x: x>3) == 4
+    assert it.first(lambda x: x > 3) == 4
     with pytest.raises(IndexError):
         Iteratable([]).first()
 
-def test_first_or_none():
-    it=Iteratable(range(10))
-    assert it.first_or_none() == 0
-    assert it.first_or_none(lambda x: x>3) == 4
 
-    assert Iteratable([]).first_or_none() == None
-    assert Iteratable([]).first_or_none(lambda x: x>3) == None
+def test_first_or_none():
+    it = Iteratable(range(10))
+    assert it.first_or_none() == 0
+    assert it.first_or_none(lambda x: x > 3) == 4
+
+    assert Iteratable([]).first_or_none() is None
+    assert Iteratable([]).first_or_none(lambda x: x > 3) is None
+
 
 def test_last():
-    it=Iteratable(range(10))
+    it = Iteratable(range(10))
     assert it.last() == 9
-    assert it.last(lambda x: x<7) == 6
-    
+    assert it.last(lambda x: x < 7) == 6
+
     with pytest.raises(IndexError):
         Iteratable([]).last()
 
-def test_last_or_none():
-    it=Iteratable(range(10))
-    assert it.last_or_none() == 9
-    assert it.last_or_none(lambda x: x>3) == 9
 
-    assert Iteratable([]).last_or_none() == None
-    assert Iteratable([]).last_or_none(lambda x: x>3) == None
+def test_last_or_none():
+    it = Iteratable(range(10))
+    assert it.last_or_none() == 9
+    assert it.last_or_none(lambda x: x > 3) == 9
+
+    assert Iteratable([]).last_or_none() is None
+    assert Iteratable([]).last_or_none(lambda x: x > 3) is None
+
 
 def test_for_each():
-    result=0
+    result = 0
+
     def accum_result(x):
         nonlocal result
         result += x
 
-    Iteratable([1,2,3,4]).for_each(accum_result)
+    Iteratable([1, 2, 3, 4]).for_each(accum_result)
     assert result == 10
 
+
 def test_take():
-    result=Iteratable([1,2,3,4])\
-           .take(3)\
-           .to_list()
-    assert len(result)==3
-    assert sum(result)==1+2+3
+    result = Iteratable([1, 2, 3, 4]).take(3).to_list()
+    assert len(result) == 3
+    assert sum(result) == 1 + 2 + 3
+
 
 def test_takewhile():
-    result=Iteratable(range(10))\
-           .takewhile(lambda x: x<5)\
-           .to_list()
-    assert len(result)==5
-    assert sum(result)==1+2+3+4
+    result = Iteratable(range(10)).takewhile(lambda x: x < 5).to_list()
+    assert len(result) == 5
+    assert sum(result) == 1 + 2 + 3 + 4
 
-def test_skip():    
-    result=Iteratable([1,2,3,4])\
-           .skip(2)\
-           .to_list()
-    assert len(result)==2
-    assert sum(result)==3+4
+
+def test_skip():
+    result = Iteratable([1, 2, 3, 4]).skip(2).to_list()
+    assert len(result) == 2
+    assert sum(result) == 3 + 4
+
 
 def test_skipwhile():
-    result=Iteratable(range(10))\
-           .skipwhile(lambda x: x<5)\
-           .to_list()
-    assert len(result)==5
-    assert sum(result)==5+6+7+8+9
+    result = Iteratable(range(10)).skipwhile(lambda x: x < 5).to_list()
+    assert len(result) == 5
+    assert sum(result) == 5 + 6 + 7 + 8 + 9
+
 
 def test_zip_iterator():
-    it1 = Iteratable([1,2,3,4])
-    it2 = Iteratable([1,2,3,4])
+    it1 = Iteratable([1, 2, 3, 4])
+    it2 = Iteratable([1, 2, 3, 4])
 
     result = it1.zip(it2).to_list()
 
@@ -117,6 +126,7 @@ def text_generator():
     def my_generator():
         for i in range(10):
             yield i
+
     a = Iteratable(my_generator())
     low = a.where(lambda x: x < 5)
 
@@ -126,13 +136,16 @@ def text_generator():
     assert low.count() == 5
     assert high.count() == 0
 
+
 def test_select_many():
-    result = Iteratable([[0,1],[0,1],[0,1]]).select_many(lambda col:col).to_list()
+    data = [[0, 1], [0, 1], [0, 1]]
+    result = Iteratable(data).select_many(lambda col: col).to_list()
 
     assert len(result) == 6
     for i in range(6):
-        assert i%2 == result[i]
+        assert i % 2 == result[i]
+
 
 def test_chunk():
     result = Iteratable(range(10)).chunk(2).to_list()
-    assert len(result)  == 5
+    assert len(result) == 5
--- tests/test_pytorch_nn_summary.py
+++ tests/test_pytorch_nn_summary.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 
 from yutils.pytorch_nn_summary import nn_summary
 
+
 class SingleInputNet(nn.Module):
     def __init__(self):
         super().__init__()
@@ -17,9 +18,7 @@ class SingleInputNet(nn.Module):
             nn.ReLU(inplace=True),
         )
         self.classifier = nn.Sequential(
-            nn.Linear(320, 50),
-            nn.ReLU(inplace=True),
-            nn.Linear(50, 10)
+            nn.Linear(320, 50), nn.ReLU(inplace=True), nn.Linear(50, 10)
         )
 
     def forward(self, x):
@@ -28,38 +27,42 @@ class SingleInputNet(nn.Module):
         x = self.classifier(x)
         return F.log_softmax(x, dim=1)
 
+
 class Lenet5(nn.Module):
     def __init__(self, num_classes, grayscale, padding=0):
         super().__init__()
 
-        self.learning_rate=0.01
+        self.learning_rate = 0.01
         self.num_classes = num_classes
 
         if grayscale:
             in_channels = 1
         else:
             in_channels = 3
-        
+
         self.features = nn.Sequential(
-            nn.Conv2d(in_channels, 6*in_channels, kernel_size=5, padding=padding),
+            nn.Conv2d(in_channels, 6 * in_channels, kernel_size=5, padding=padding),
             nn.Tanh(),
             nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(6*in_channels, 16*in_channels, kernel_size=5),
+            nn.Conv2d(6 * in_channels, 16 * in_channels, kernel_size=5),
             nn.Tanh(),
-            nn.MaxPool2d(kernel_size=2))
+            nn.MaxPool2d(kernel_size=2),
+        )
 
         self.classifier = nn.Sequential(
-            nn.Linear(16*5*5*in_channels, 120*in_channels),
+            nn.Linear(16 * 5 * 5 * in_channels, 120 * in_channels),
             nn.Tanh(),
-            nn.Linear(120*in_channels, 84*in_channels),
+            nn.Linear(120 * in_channels, 84 * in_channels),
             nn.Tanh(),
-            nn.Linear(84*in_channels, num_classes))        
+            nn.Linear(84 * in_channels, num_classes),
+        )
 
     def forward(self, x):
         x = self.features(x)
         x = torch.flatten(x, 1)
         x = self.classifier(x)
         return x
+
 
 class MultipleInputNet(nn.Module):
     def __init__(self):
@@ -78,13 +81,14 @@ class MultipleInputNet(nn.Module):
         x = torch.cat((x1, x2), 0)
         return F.log_softmax(x, dim=1)
 
+
 def check_result_integrity(summary_str, summary):
     print(type(summary_str) == str)
     print(type(summary) == list)
-    print('name' in summary[0])
-    print('output_shape' in summary[0])
-    print('input_shape' in summary[0])
-    print('trainable' in summary[0])
+    print("name" in summary[0])
+    print("output_shape" in summary[0])
+    print("input_shape" in summary[0])
+    print("trainable" in summary[0])
 
 
 def test_single_input():
@@ -92,15 +96,16 @@ def test_single_input():
     summary_str, summary = nn_summary(SingleInputNet(), input)
     check_result_integrity(summary_str, summary)
 
+
 def test_multi_input():
     input1 = (1, 300)
     input2 = (1, 300)
     summary_str, summary = nn_summary(MultipleInputNet(), [input1, input2])
     check_result_integrity(summary_str, summary)
 
+
 def test_lenet5():
     model = Lenet5(10, True)
     input = (1, 32, 32)
     summary_str, summary = nn_summary(model, input)
-    check_result_integrity(summary_str, summary)    
-
+    check_result_integrity(summary_str, summary)
